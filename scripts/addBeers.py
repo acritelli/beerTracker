017b14ead5a2e27bc6c2080ed211aa1629f7c5cc
acritelli <critellia@gmail.com>
2017-09-07 21:59:21 -0400

    fixed bug in addBeers script, added a new list
    - Added a continue and iterator incrementation for addBeers
    - Added the list for the 2017 Tap it Forward Festival
        - Breweries only, no beers were announced

--- scripts/addBeers.py
+++ scripts/addBeers.py
@@ -1,13 +1,13 @@
 # Imports all breweries and beers from a CSV file
 # CSV file should be formatted as: Brewery, Beer 1, Beer 2, ..., Beer N
 # IF brewery has no listed beer, then format: Brewery,
-
+import sys
 from pymongo import MongoClient
 client = MongoClient()
 db = client.beerexpo
 collection = db.defBeers
 
-f = open('flour_city_list.txt')
+f = open(sys.argv[1])
 
 for line in f:
     line = line.strip('\n')
@@ -21,13 +21,15 @@ for line in f:
         # Remove . and $ characters from brewery and beer, since they're keys in Mongo
         item = item.replace('.', '')
         item = item.replace('$', '')
-        
+
         if item == '':
-            next
+            i += 1
+            continue
 
         if i == 0:
             document['name'] = item
         else:
             document['beers'].append(item)
         i += 1
+        print(document)
     collection.insert_one(document)
